ad1d5ee88c9a408701f2f2bed252e4fb63738296
Brandon <bcantr3ll@gmail.com>
2018-08-07 21:45:06 -0500

    Initial commit

--- getstuff.py
+++ getstuff.py
@@ -11,7 +11,7 @@ headers = {'Content-Type': 'application/json',
            'Authorization': f'{api_token}'}
 
 def get_guild():
-    api_url = 'https://www.warcraftlogs.com:443/v1/reports/guild/matt%20damon/arthas/US?api_key=c5eec3055c6db89b97b61ce74cf4eb1b'
+    api_url = f'https://www.warcraftlogs.com:443/v1/reports/guild/matt%20damon/arthas/US?api_key={api_token}'
     response = requests.get(api_url, headers=headers)
     info = json.loads(response.content.decode('utf-8'))
     return info
