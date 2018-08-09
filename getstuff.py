1f304ff5b9e8dac64fb4a223e73ad908c7cba37a
Brandon <bcantr3ll@gmail.com>
2018-08-08 19:16:00 -0500

    added date stuff

--- getstuff.py
+++ getstuff.py
@@ -2,16 +2,25 @@
 #Working with warcraftlogs API to play around
 import json
 import requests
-from datetime import date
+from datetime import datetime as dt, timedelta
 
-api_token = 'redacted'
+#Datemath bullshit
+last_week = dt.now() - timedelta(days=7)
+now_int = int(dt.now().timestamp() * 1000)
+last_week_int = int(last_week.timestamp() * 1000)
+
+#API Variables
+api_token = 'c5eec3055c6db89b97b61ce74cf4eb1b'
 api_url_base = 'https://www.warcraftlogs.com/v1/'
+guild = 'matt damon'
+realm = 'arthas'
+region = 'US'
 
 headers = {'Content-Type': 'application/json',
            'Authorization': f'{api_token}'}
 
 def get_guild():
-    api_url = f'https://www.warcraftlogs.com:443/v1/reports/guild/matt%20damon/arthas/US?api_key={api_token}'
+    api_url = f'https://www.warcraftlogs.com:443/v1/reports/guild/{guild}/{realm}/{region}?start={last_week_int}&end={now_int}&api_key={api_token}'
     response = requests.get(api_url, headers=headers)
     info = json.loads(response.content.decode('utf-8'))
     return info
